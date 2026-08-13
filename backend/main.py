6ee92a9fb48c32884c260f62b8b9ed1db7446f6b
jashtejani2608@gmail.com <jashtejani2608@gmail.com>
2026-08-13 17:43:43 +0530

    middleware added

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 import tempfile
 from pdf_parser import extract_text_from_pdf
 
 app = FastAPI()
 
+app.add_middleware (
+    CORSMiddleware,
+    allow_origins=[
+        "https://localhost:5173",
+        "https://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
